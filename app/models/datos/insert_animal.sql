2a385892bda26f9fdc0a9efe629437863c3654de
Nuria Díaz Sánchez <nuriadiazsanchez91@gmail.com>
2023-05-23 21:39:27 +0200

    Scrip insert mascotas y cambio nom a img gatos

--- app/models/datos/insert_animal.sql
+++ app/models/datos/insert_animal.sql
@@ -1,8 +1,21 @@
-insert into animal (microchip, nif_refugio, especie, raza, nombre, sexo, fecha_nac, tamano, peso) values ('573919332786424', 'K56699163', 'Perro', 'Golden Retriever', 'Reinhold', 'macho', '2019/14/8', 'mediano', 16.8);
-insert into animal (microchip, nif_refugio, especie, raza, nombre, sexo, fecha_nac, tamano, peso) values ('084936874255346', 'V46094192', 'Ave', 'Canario', 'Glenden', 'macho', '2021/8/5', 'mediano', 12.9);
+--INSERT GATOS
+insert into animal (microchip, nif_refugio, especie, raza, nombre, sexo, fecha_nac, tamano, peso) values ('859722090811891', 'K56699163', 'gato', 'Común Europeo', 'Croqueta', 'macho', '2019/12/8', 'mediano', 16.8);
+insert into animal (microchip, nif_refugio, especie, raza, nombre, sexo, fecha_nac, tamano, peso) values ('650001299838843', 'V46094192', 'gato', 'Común Europeo', 'Dominika', 'macho', '2019/4/8', 'mediano', 16.8);
+insert into animal (microchip, nif_refugio, especie, raza, nombre, sexo, fecha_nac, tamano, peso) values ('478567986302224', 'A39273608', 'gato', 'Común Europeo', 'Dorme', 'macho', '2019/1/8', 'mediano', 16.8);
+insert into animal (microchip, nif_refugio, especie, raza, nombre, sexo, fecha_nac, tamano, peso) values ('744612778330798', 'K56699163', 'gato', 'Común Europeo', 'Fireball', 'macho', '2019/5/8', 'mediano', 16.8);
+insert into animal (microchip, nif_refugio, especie, raza, nombre, sexo, fecha_nac, tamano, peso) values ('221852759130408', 'E70999499', 'gato', 'Común Europeo', 'Jagger', 'macho', '2019/4/8', 'mediano', 16.8);
+insert into animal (microchip, nif_refugio, especie, raza, nombre, sexo, fecha_nac, tamano, peso) values ('968393970949002', 'Q06296670', 'gato', 'Común Europeo', 'Marga', 'macho', '2019/2/8', 'mediano', 16.8);
+insert into animal (microchip, nif_refugio, especie, raza, nombre, sexo, fecha_nac, tamano, peso) values ('024173129510892', 'K56699163', 'gato', 'Común Europeo', 'Mojo', 'macho', '2019/3/8', 'mediano', 16.8);
+insert into animal (microchip, nif_refugio, especie, raza, nombre, sexo, fecha_nac, tamano, peso) values ('112112359033368', 'Q06296670', 'gato', 'Común Europeo', 'Vento', 'macho', '2019/9/8', 'mediano', 16.8);
 insert into animal (microchip, nif_refugio, especie, raza, nombre, sexo, fecha_nac, tamano, peso) values ('350401816540484', 'V46094192', 'Gato', 'Siamés', 'Ingeberg', 'hembra', '2017/6/5', 'Mini', 1.8);
 insert into animal (microchip, nif_refugio, especie, raza, nombre, sexo, fecha_nac, tamano, peso) values ('679206584331511', 'E70999499', 'Gato', 'Común Europeo', 'Pattie', 'macho', '2015/5/27', 'pequeño', 7.6);
+
+--INSERT PERROS
+insert into animal (microchip, nif_refugio, especie, raza, nombre, sexo, fecha_nac, tamano, peso) values ('573919332786424', 'K56699163', 'Perro', 'Golden Retriever', 'Reinhold', 'macho', '2019/6/8', 'mediano', 16.8);
 insert into animal (microchip, nif_refugio, especie, raza, nombre, sexo, fecha_nac, tamano, peso) values ('283805332074835', 'A39273608', 'Perro', 'mestizo', 'Eliza', 'hembra', '2019/4/9', 'Pequeño', 5.3);
+insert into animal (microchip, nif_refugio, especie, raza, nombre, sexo, fecha_nac, tamano, peso) values ('294939551137618', 'Q06296670', 'Perro', 'mestizo', 'Orton', 'macho', '2017/2/24', 'pequeño', 6.6);
+
+--INSERT OTROS
+insert into animal (microchip, nif_refugio, especie, raza, nombre, sexo, fecha_nac, tamano, peso) values ('084936874255346', 'V46094192', 'Ave', 'Canario', 'Glenden', 'macho', '2021/8/5', 'mediano', 12.9);
 insert into animal (microchip, nif_refugio, especie, raza, nombre, sexo, fecha_nac, tamano, peso) values ('378181954075169', 'V46094192', 'Conejo', 'Enano', 'Vickie', 'hembra', '2022/8/12', 'mediano', 15.6);
 insert into animal (microchip, nif_refugio, especie, raza, nombre, sexo, fecha_nac, tamano, peso) values ('525922593452408', 'K56699163', 'Hurón', 'Hurón', 'Arty', 'macho', '2018/3/13', 'mediano', 16.1);
-insert into animal (microchip, nif_refugio, especie, raza, nombre, sexo, fecha_nac, tamano, peso) values ('294939551137618', 'Q06296670', 'Perro', 'mestizo', 'Orton', 'macho', '2017/2/24', 'pequeño', 6.6);
